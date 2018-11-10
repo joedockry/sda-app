d4c1037fdec938b192c3eeccca6562f71660eb15
jdockry1 <32038395+jdockry1@users.noreply.github.com>
2018-11-10 11:46:30 +0000

    Added PHP for Heroku deployment

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>SEAN DOCKRY & ASSOCIATES</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<!--<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico"/>-->
+		<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16">
+        <link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script>
+		</script>
+    </head>
+    <body>
+        <header id="navigation-bar">
+			<nav>
+				<logo></logo>
+				<div class="logo-fixed">Sea&#769;n Dockry and Associates</div>
+				<div class="tabs">
+					<ul class="tabs-list">
+					  	<li id="home" class="tab selected-tab"><a class="tab-link" href="test.html">Home</a></li><!--
+				   		--><li id="services" class="tab"><a class="tab-link" href="services.html">Services</a></li><!--
+				   		--><li id="about" class="tab"><a class="tab-link" href="about.html">About</a></li><!--
+				   		--><li id="contact" class="tab"><a class="tab-link" href="contact.html">Contact</a></li>
+					  	<hr />
+					</ul>
+				</div>
+			</nav>
+			<burger>
+				<input type="checkbox" />
+				<span></span>
+				<span></span>
+				<span></span>
+				<ul class="burger-menu">
+					<li id="burger-home" class="burger-tab burger-selected-item"><a class="burger-tab-link" href="test.html">Home</a></li>
+					<li id="burger-services" class="burger-tab"><a class="burger-tab-link" href="services.html">Services</a></li>
+					<li id="burger-about" class="burger-tab"><a class="burger-tab-link" href="about.html">About</a></li>
+					<li id="burger-contact" class="burger-tab"><a class="burger-tab-link" href="contact.html">Contact</a></li>
+					<div id="burger-select"></div>
+				</ul>
+			</burger>
+		</header>
+        <div class="container">
+			<!--<div class="carousel-container">
+				<ul class="carousel">
+					<input type="radio" id="carousel-1" name="activator" checked="checked" class="carousel-activator"/>
+					<input type="radio" id="carousel-2" name="activator" class="carousel-activator"/>
+					<input type="radio" id="carousel-3" name="activator" class="carousel-activator"/>
+					<div class="carousel-controls controls-1">
+						<label for="carousel-2" class="carousel-control carousel-control-forward"></label>
+						<label for="carousel-3" class="carousel-control carousel-control-backward"></label>
+					</div>
+					<div class="carousel-controls controls-2">
+						<label for="carousel-3" class="carousel-control carousel-control-forward"></label>
+						<label for="carousel-1" class="carousel-control carousel-control-backward"></label>
+					</div>
+					<div class="carousel-controls controls-3">
+						<label for="carousel-1" class="carousel-control carousel-control-forward"></label>
+						<label for="carousel-2" class="carousel-control carousel-control-backward"></label>
+					</div>
+					<div class="carousel-track">
+						<div class="carousel-slide carousel-1">
+							<div class="carousel-content">
+								<div class="carousel-header">Title 1</div>
+								<div class="carousel-body">content</div>
+							</div>
+						</div>
+						<div class="carousel-slide carousel-2">
+							<div class="carousel-content">
+								<div class="carousel-header">Title 2</div>
+								<div class="carousel-body">content</div>
+							</div>
+						</div>
+						<div class="carousel-slide carousel-3">
+							<div class="carousel-content">
+								<div class="carousel-header">Title 3</div>
+								<div class="carousel-body">content</div>
+							</div>
+						</div>
+					</div>
+					<div class="carousel-indicators">
+						<label for="carousel-1" class="carousel-indicator indicator-1"></label>
+						<label for="carousel-2" class="carousel-indicator indicator-2"></label>
+						<label for="carousel-3" class="carousel-indicator indicator-3"></label>
+					</div>
+				</ul>
+			</div>-->
+			<div class="page-title">
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Projects
+				<hr>
+			</div>
+			<div class="grid">
+				<div class="row"> 
+					<div class="column">
+						<div class="grid-item">
+							<a href="projects/category.html?category=alterations-extensions-dwellings">
+								<img src="projects/alterations-extensions-dwellings/cover.jpg">
+								<div>Alterations & <br>Extensions to <br>Dwellings</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=apartments">
+								<img src="projects/apartments/cover.jpg">
+								<div>Apartments</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=dwellings-large">
+								<img src="projects/dwellings-large/cover.jpg">
+								<div>Dwellings Large</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=dwellings">
+								<img src="projects/dwellings/cover.jpg">
+								<div>Dwellings</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=education">
+								<img src="projects/education/cover.jpg">
+								<div>Education</div>
+							</a>
+						</div>
+					</div>
+					<div class="column">
+						<div class="grid-item">
+							<a href="projects/category.html?category=healthcare">
+								<img src="projects/healthcare/cover.jpg">
+								<div>Healthcare</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=heritage-conservation">
+								<img src="projects/heritage-conservation/cover.jpg">
+								<div>Heritage Conservation</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=hotels">
+								<img src="projects/hotels/cover.jpg">
+								<div>Hotels</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=housing-estates">
+								<img src="projects/housing-estates/cover.jpg">
+								<div>Housing Estates</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=multiuse">
+								<img src="projects/multiuse/cover.jpg">
+								<div>Multiuse</div>
+							</a>
+						</div>
+					</div>
+					<div class="column">
+						<div class="grid-item">
+							<a href="projects/category.html?category=offices">
+								<img src="projects/offices/cover.jpg">
+								<div>Offices</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=pubs-restaurants">
+								<img src="projects/pubs-restaurants/cover.jpg">
+								<div>Pubs & Restaurants</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=retail-large">
+								<img src="projects/retail-large/cover.jpg">
+								<div>Retail Large</div>
+							</a>
+						</div>
+						<div class="grid-item">
+							<a href="projects/category.html?category=retail-highstreet">
+								<img src="projects/retail-highstreet/cover.jpg">
+								<div>Retail Highstreet</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+        <footer>
+			<div class="footer-title">
+				Sea&#769;n Dockry and Associates
+			</div>
+			<div class="footer-social">
+				<i class="fa fa-facebook-square social-media" style="font-size:65px"></i>
+				<i class="fa fa-twitter-square social-media" style="font-size:65px"></i>
+				<i class="fa fa-envelope-square social-media" style="font-size:65px"></i>
+			</div>
+			<div class="footer-bottom">
+				<div class="footer-info">
+					<div class="icon">
+						<center>
+							<i class="fa fa-map-marker" style="font-size:24px"></i>
+						</center>
+					</div>
+					<div>Second Floor, Corbett House, Shoemaker Lane, Edward Square, Galway</div>
+				</div>
+				<div class="footer-info">
+					<div class="icon">
+						<center>
+							<i class="fa fa-phone" style="font-size:24px"></i>
+						</center>
+					</div>
+					<div>+353 (0)91 568522</div>
+				</div>
+				<div class="footer-info">
+					<div class="icon">
+						<center>
+							<i class="fa fa-envelope" style="font-size:24px"></i>
+						</center>
+					</div>
+					<div>mail@sdaarchitects.ie</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+	</html>
+	<script>
+
+		/*
+		Sticky Header Controller
+		*/
+
+		var header = document.getElementById("navigation-bar");
+		var sticky = header.offsetTop + 40;
+		var touch = header.offsetTop + 15;
+
+		$(document.body).on('touchmove', onTouch);
+		$(window).scroll(onScroll); 
+
+		function onScroll(){ 
+			if (window.pageYOffset >= sticky) {
+				header.classList.add("nav-fixed");
+			} else {
+				header.classList.remove("nav-fixed");
+			}
+		}
+
+		function onTouch(){ 
+			if (window.pageYOffset >= touch) {
+				header.classList.add("nav-fixed");
+			} else {
+				header.classList.remove("nav-fixed");
+			}
+		}
+
+
+
+		/*
+        Carousel Resize
+        */
+        $(window).ready(updateHeight);
+        $(window).resize(updateHeight);
+
+        function updateHeight() {
+            var div = $('.carousel-container');
+            var width = div.width();
+            
+            div.css('height', width * 0.5625);
+        }
+
+	</script>
+</html>
